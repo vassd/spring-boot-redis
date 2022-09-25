701b693de9f85438f88e35abcbddbedb37de7dff
David Vass <nighturbex@protonmail.com>
2022-09-25 15:57:18 +0200

    feat: add GET/DELETE for one user, filterEndpoint

--- src/main/java/com/example/redisspringboot/controller/UserController.java
+++ src/main/java/com/example/redisspringboot/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.redisspringboot.model.User;
 import com.example.redisspringboot.repository.UserRepository;
 import com.example.redisspringboot.service.UserService;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,13 +44,24 @@ public class UserController {
   private static final String USERS_ENDPOINT = "/users";
 
   /**
-   * Gets all user.
+   * Gets all users.
    *
-   * @return the all user
+   * @return All users
    */
   @GetMapping(USERS_ENDPOINT)
   public List<UserEntity> getAllUser() {
     return userRepository.findAll();
+  }
+
+  /**
+   * Gets user.
+   *
+   * @param userId the user id
+   * @return the user
+   */
+  @GetMapping(USERS_ENDPOINT + "/{id}")
+  public Optional<UserEntity> getUser(@PathVariable("id") final String userId) {
+    return userRepository.findById(UUID.fromString(userId));
   }
 
   /**
@@ -67,7 +81,7 @@ public class UserController {
    * Update user entity.
    *
    * @param userId the id
-   * @param user the user
+   * @param user   the user
    * @return the response entity
    */
   @PatchMapping(USERS_ENDPOINT)
@@ -76,11 +90,21 @@ public class UserController {
   }
 
   /**
-   * Delete users.
+   * Delete all users.
    */
   @DeleteMapping(USERS_ENDPOINT)
   public void deleteUsers() {
     userRepository.deleteAll();
+  }
+
+  /**
+   * delete user.
+   *
+   * @param userId the user id
+   */
+  @DeleteMapping(USERS_ENDPOINT + "/{id}")
+  public void deleteUser(@PathVariable("id") final String userId) {
+    userRepository.deleteById(UUID.fromString(userId));
   }
 
   /**
@@ -92,5 +116,16 @@ public class UserController {
   public List<UserEntity> getSorted() {
 
     return userRepository.findAll(Sort.by(Sort.Direction.DESC, "age"));
+  }
+
+  /**
+   * Gets all user whose age is over 30.
+   *
+   * @return filtered users
+   */
+  @GetMapping("/usersOver30")
+  public List<UserEntity> getUsersOver30() {
+
+    return userRepository.findAll().stream().filter(user -> user.getAge() > 30).toList();
   }
 }
